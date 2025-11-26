3ed655f11cc9055da46315f7971e2d5450a06262
0nepeop1e <engshun.lim@gmail.com>
2025-11-27 05:33:51 +0800

    fix format and svelte check

--- src/routes/manifest.webmanifest/+server.ts
+++ src/routes/manifest.webmanifest/+server.ts
@@ -3,23 +3,26 @@ import faviconSvg from '$lib/assets/favicon.svg?url';
 export const prerender = true;
 
 export function GET() {
-    return Response.json({
-        name: 'Random Tools',
-        short_name: 'RandomTools',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#fcfcff',
-        theme_color: '#fcfcff',
-        icons: [
-            {
-                src: faviconSvg,
-                sizes: 'any',
-                type: 'image/svg+xml'
-            }
-        ]
-    }, {
-        headers: {
-            'Content-Type': 'application/manifest+json;charset=UTF-8'
-        }
-    });
+	return Response.json(
+		{
+			name: 'Random Tools',
+			short_name: 'RandomTools',
+			start_url: '/',
+			display: 'standalone',
+			background_color: '#fcfcff',
+			theme_color: '#fcfcff',
+			icons: [
+				{
+					src: faviconSvg,
+					sizes: 'any',
+					type: 'image/svg+xml'
+				}
+			]
+		},
+		{
+			headers: {
+				'Content-Type': 'application/manifest+json;charset=UTF-8'
+			}
+		}
+	);
 }
