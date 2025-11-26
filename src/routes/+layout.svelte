7d465cf06d62d5bffa208a65b65e720faaf43af4
0nepeop1e <engshun.lim@gmail.com>
2025-11-27 05:07:13 +0800

    web manifest

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,16 +2,23 @@
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import { theme } from '$lib/theme.svelte';
+	import { browser } from '$app/environment';
 
 	let { children } = $props();
 
-	$effect(() => {
-		if (theme.current === 'dark') {
-			document.documentElement.classList.add('dark');
-		} else {
-			document.documentElement.classList.remove('dark');
-		}
-	});
+	if (browser) {
+		const themeColor = document.querySelector('meta[name="theme-color"]');
+
+		$effect(() => {
+			if (theme.current === 'dark') {
+				document.documentElement.classList.add('dark');
+				themeColor?.setAttribute('content', '#131119');
+			} else {
+				document.documentElement.classList.remove('dark');
+				themeColor?.setAttribute('content', '#f9f9ff');
+			}
+		});
+	}
 </script>
 
 <svelte:head>
